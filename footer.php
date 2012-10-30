55217fc1745605a461feba17cee2a24f66764133
Brian Richards <brian@rzen.net>
2012-10-29 23:46:10 -0400

    Iinitial, minor HTML5 tweaks to get our document in line

--- footer.php
+++ footer.php
@@ -3,28 +3,28 @@
 </div><!-- #wrap .hfeed -->
 <?php do_action( 'sb_between_content_and_footer' ); ?>
 <div id="footer_wrap">
-	
+
 	<?php do_action( 'sb_before_footer' ); ?>
-	
-	<div id="footer">
-		
+
+	<footer id="footer" class="site-footer" role="contentinfo">
+
 		<?php
 			do_action( 'sb_footer_widgets' );
 			do_action( 'sb_footer' );
-			if ( has_action( 'wp_footer' ) ) { 
+			if ( has_action( 'wp_footer' ) ) {
 				echo '<div id="wp_footer">';
 				wp_footer();
 				echo '</div><!-- #wp_footer -->';
 			}
 		?>
-		
-	</div><!-- #footer -->
-	
+
+	</footer><!-- #footer -->
+
 	<?php do_action( 'sb_after_footer' ); ?>
-	
+
 </div><!-- #footer_wrap .hfeed -->
 
 <?php do_action( 'sb_after' ); ?>
-	
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
-<head profile="http://gmpg.org/xfn/11">
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
 	<title><?php wp_title(); ?></title>
-	<meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-	
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/includes/scripts/html5.js" type="text/javascript"></script>
+	<![endif]-->
+
 	<!-- BEGIN wp_head() -->
 	<?php wp_head(); ?>
 	<!-- END wp_head() -->
@@ -12,16 +18,16 @@
 <body <?php body_class(); ?>>
 <?php do_action( 'sb_before' ); ?>
 <div id="wrap" class="hfeed">
-	
+
 	<?php do_action( 'sb_before_header' ); ?>
-	
+
 	<?php if ( has_action( 'sb_header' ) ) { ?>
-		<div id="header">
+		<header id="header" class="site-header" role="banner">
 			<?php do_action( 'sb_header' ); ?>
-		</div><!-- #header -->
+		</header><!-- #header .site-header -->
 	<?php } ?>
-	
+
 	<?php do_action( 'sb_after_header' ); ?>
-	
+
 	<div id="container_wrap">
 		<?php do_action( 'sb_before_container' ); ?>
